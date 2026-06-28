dcd78f76be105850f67e7307a37986fd47a4512f
Jaqueline Atienza <jaquibatienza@gmail.com>
2026-06-27 21:25:08 -0300

    se añadio nueva ruta para obetener el progreso por id del libro

--- src/userPogressBooks/interface/routes/bookProgress.routes.ts
+++ src/userPogressBooks/interface/routes/bookProgress.routes.ts
@@ -5,11 +5,12 @@ import { saveBookProgress } from "../controllers/bookSaveProgress.controllers";
 import { updateProgresBook } from "../controllers/updateProgress.controllers";
 import { deleteProgresBook } from "../controllers/deleteProgress.controllers";
 import { validateJWT } from "../../../shared/middlewares/validateJWT";
-import { findByProgressIdControllers } from "../controllers/findProgress";
+import { findByProgressIdControllers, findProgressByBook } from "../controllers/findProgress";
 
 
 progressRouter.post('/SaveProgress', validateJWT, saveBookProgress)
 progressRouter.get('/Progress', validateJWT, findByProgressIdControllers)
+progressRouter.get("/progress/id", validateJWT, findProgressByBook)
 progressRouter.put('/progress', validateJWT, updateProgresBook)
 progressRouter.delete('/progress', validateJWT, deleteProgresBook)
 
